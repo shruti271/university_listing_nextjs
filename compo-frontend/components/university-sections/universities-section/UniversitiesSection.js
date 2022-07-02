05751f3c998f7593f6a58957e8f2a9daaab5bebd
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-07-03 01:01:58 +0600

    Universities: Add hero section

---
+++ compo-frontend/components/university-sections/universities-section/UniversitiesSection.js
@@ -0,0 +1,9 @@
+const UniversitiesSection = () => {
+  return (
+    <section>
+      <h1>Unis</h1>
+    </section>
+  );
+};
+
+export default UniversitiesSection;
---
+++ compo-frontend/pages/universities/index.js
@@ -0,0 +1,13 @@
+import HeroSection from '../../components/university-sections/hero-section/HeroSection';
+import UniversitiesSection from '../../components/university-sections/universities-section/UniversitiesSection';
+
+const Universities = () => {
+  return (
+    <>
+      <HeroSection />
+      <UniversitiesSection />
+    </>
+  );
+};
+
+export default Universities;
