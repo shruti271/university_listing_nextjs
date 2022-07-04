c010dfbed5742526b4827fb507879101365db98b
denisgodhani <godhanidenis@gmail.com>
2022-07-04 14:56:37 +0530

    Fixed Layout issues

--- compo-frontend/tailwind.config.js
+++ compo-frontend/tailwind.config.js
@@ -1,21 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
-        colorPrimary: '#0364FF',
-        colorSecondary: '#EB6B09',
-        colorBlack: '#000000',
-        colorWhite: '#FFFFFF',
-        colorStone: '#544E5D',
-        colorPale: '#F9F9FA',
-        colorTransparent: 'transparent',
+        colorPrimary: "#0364FF",
+        colorSecondary: "#EB6B09",
+        colorBlack: "#000000",
+        colorWhite: "#FFFFFF",
+        colorStone: "#544E5D",
+        colorPale: "#F9F9FA",
+        colorTransparent: "transparent",
       },
     },
   },
-  plugins: [],
+  plugins: [require("tw-elements/dist/plugin")],
 };
