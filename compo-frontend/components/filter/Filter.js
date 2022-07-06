8b252fac8b9a7c2c74e9551b0f13534c4a4dfd08
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-07-06 19:11:24 +0600

    Set the base for filters

--- compo-frontend/components/filter/Filter.js
+++ compo-frontend/components/filter/Filter.js
@@ -1,6 +1,11 @@
+import CampusSetting from './campus-setting/CampusSetting';
+import CollegeType from './CollegeType/CollegeType';
 import Cost from './cost/Cost';
 import InstituteType from './institute-type/InstituteType';
+import Majors from './majors/Majors';
 import SearchLocation from './search-location/SearchLocation';
+import Selectivity from './selectivity/Selectivity';
+import StudentPopulation from './StudentPupulation/StudentPopulation';
 
 const Filter = () => {
   return (
@@ -8,6 +13,11 @@ const Filter = () => {
       <SearchLocation />
       <InstituteType />
       <Cost />
+      <Majors />
+      <Selectivity />
+      <CampusSetting />
+      <StudentPopulation />
+      <CollegeType />
     </div>
   );
 };
--- compo-frontend/pages/index.js
+++ compo-frontend/pages/index.js
@@ -2,7 +2,7 @@ import AboutSection from '../components/sections/about-section/AboutSection';
 import FeaturesSection from '../components/sections/features-section/FeaturesSection';
 import GetStartedSection from '../components/sections/get-started-section/GetStartedSection';
 import HeroSection from '../components/sections/hero-section/HeroSection';
-import NewsletterSection from '../components/sections/newsletter-section/NewsletterSection';
+import NewsletterSection from '../components/newsletter-section/NewsletterSection';
 import OffersSection from '../components/sections/offers-section/OffersSection';
 import PathsSection from '../components/sections/paths-section/PathsSection';
 import TestimonialsSection from '../components/sections/testimonials-section/TestimonialsSection';
