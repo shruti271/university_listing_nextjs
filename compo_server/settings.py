c8babbfc0bbb27c58857d1271aa313e00f6fc673
Mohamed <mohamed.elfirdoussi@um5r.ac.ma>
2022-07-08 10:57:52 +0100

    Add cors to allow access from other origins

--- compo_server/settings.py
+++ compo_server/settings.py
@@ -108,6 +108,7 @@ INSTALLED_APPS = [
     'accounts',
     'rest_framework_simplejwt',
     'directory',
+    'corsheaders',
 ]
 
 # Simple JWT config
@@ -138,12 +139,15 @@ AUTHENTICATION_BACKENDS = ('accounts.AuthBack.EmailBackend',)
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'compo_server.urls'
 
