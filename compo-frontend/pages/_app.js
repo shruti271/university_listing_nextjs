a68c5bf8c1a3be2b58bba6cc2ed9073e2cd019fd
composite-git <admin@composite.digital>
2022-07-10 00:30:34 +0400

    Add google analytics

--- compo-frontend/pages/_app.js
+++ compo-frontend/pages/_app.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import {  store } from "../redux/store";
 import { Provider } from 'react-redux';
+import Script from 'next/script';
 
 import Footer from '../components/Footer';
 import Header from '../components/Header';
@@ -11,6 +12,15 @@ import "animate.css";
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Script src={`https://www.googletagmanager.com/gtag/js?id=G-YQTS4N0T2M`} strategy="afterInteractive"/>
+      <Script id="google-analytics" strategy="afterInteractive">
+      {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-YQTS4N0T2M');
+      `}
+      </Script>
       <Provider store={getStore(pageProps.initialState)}>
         <Head>
           <title>Composite</title>
