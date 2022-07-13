16b100f9e35e8e1dee908bbaf137cb85306115b7
godhanidenis <godhanidenis@gmail.com>
2022-07-13 18:38:04 +0530

    uni details

--- compo-frontend/components/core/CustomForms.js
+++ compo-frontend/components/core/CustomForms.js
@@ -2,6 +2,7 @@ import { styled } from "@mui/material/styles";
 import TextField from "@mui/material/TextField";
 
 import NativeSelect from '@mui/material/NativeSelect';
+import { LinearProgress, linearProgressClasses, Tabs } from "@mui/material";
 
 export const CustomTextField = styled(TextField)(({ theme }) => ({
   [`& .MuiInput-input`]: {
@@ -47,3 +48,52 @@ export const CustomSelect = styled(NativeSelect)(({ theme }) => ({
     color: "#544E5D",
   },
 }));
+
+
+
+export const CustomTab = styled(Tabs)(({ theme }) => ({
+  [`& .MuiTab-root`]: {
+    textTransform: "none",
+    color: "#959595",
+    fontWeight: 500,
+    fontSize: "14px",
+    lineHeight: "21px",
+    minHeight: "55px",
+    justifyContent:"start",
+    width:"100%"
+  },
+  [`& .Mui-selected`]: {
+    color: "#0364FF",
+    fontWeight: 600,
+    fontSize: "14px",
+    lineHeight: "21px",
+
+  },
+  [`& .MuiTabs-indicator`]: {
+    left: 0,
+    display: "flex",
+    justifyContent: "center",
+    backgroundColor: "#0364FF",
+    maxHeight: 25,
+    alignItems: "center",
+    marginTop: 15,
+    borderRadius: "0px 100px 100px 0px"
+  },
+
+  [`& .MuiTabs-flexContainer`]: {
+    alignItems: "start"
+  }
+}));
+
+
+export const CustomBorderLinearProgress = styled(LinearProgress)(({ theme ,setColor}) => ({
+  height: 10,
+  borderRadius: 5,
+  backgroundColor: "#F9F9FA",
+
+  [`& .${linearProgressClasses.bar}`]: {
+    borderRadius: 5,
+    backgroundColor: setColor,
+  },
+}));
+
