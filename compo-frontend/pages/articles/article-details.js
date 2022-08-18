1f6732d97d42b12874f6bcd49bff72eff3aea838
godhanidenis <godhanidenis@gmial.com>
2022-08-18 18:23:57 +0530

    fix font sizes

--- compo-frontend/pages/articles/article-details.js
+++ compo-frontend/pages/articles/article-details.js
@@ -37,14 +37,13 @@ const ArticleDetails = () => {
 
           <div className="mt-8 gap-4 flex">
             <Avatar
-              //   src={ArtProfile}
-              src="/assets/art-profile.jpg"
-              alt="ArtProfile"
               sx={{
                 width: "50px",
                 height: "50px",
               }}
-            />
+            >
+              <Image src={ArtProfile} alt="ArtProfile" layout="fill" />
+            </Avatar>
             <div className="flex flex-col justify-center">
               <p className="font-black text-base text-[#000000]">John Doe</p>
               <p className="font-normal text-sm leading-[18px] opacity-50 text-[#000000]">
@@ -55,24 +54,24 @@ const ArticleDetails = () => {
             </div>
           </div>
 
-          <p className="mt-9 w-[175px] flex flex-wrap text-[#000000] font-normal text-xl font-['PT_Serif']">
+          <p className="mt-9 w-[150px] sm:w-[175px] flex flex-wrap text-[#000000] font-normal text-md sm:text-lg md:text-xl font-['PT_Serif']">
             Lorem ipsum, everyone 👏
           </p>
-          <p className="mt-6 flex flex-wrap text-[#000000] font-normal text-xl font-['PT_Serif']">
+          <p className="mt-6 flex flex-wrap text-[#000000] font-normal text-md sm:text-lg md:text-xl font-['PT_Serif']">
             Lacinia ut gravida tempus sollicitudin condimentum sit urna cursus
             malesuada. Eu lacus dis et tempor non. Odio tristique dignissim
             lorem ipsum vestibulum. Cras quisque praesent.
           </p>
-          <div className="mt-6 font-normal text-xl text-[#000000] font-['PT_Serif']">
+          <ol className="mt-6 font-normal text-md sm:text-lg md:text-xl text-[#000000] font-['PT_Serif'] ">
             <li>Lacinia ut gravida tempus sollicitudin;</li>
             <li>Terima kasih banyak;</li>
             <li>Lorem lorem lorem</li>
             <li>Something cool here goes</li>
-          </div>
+          </ol>
           <p className="mt-4 font-bold text-xl sm:text-2xl leading-[38px] text-[#000000]">
             1. What is Stash
           </p>
-          <p className="mt-3 text-xl text-[#000000] font-normal font-['PT_Serif']">
+          <p className="mt-3 text-md sm:text-lg md:text-xl text-[#000000] font-normal font-['PT_Serif']">
             Lacinia ut gravida tempus sollicitudin condimentum sit urna cursus
             malesuada. Eu lacus dis et tempor non. Odio tristique dignissim
             lorem ipsum vestibulum. Cras quisque praesent.
@@ -81,7 +80,7 @@ const ArticleDetails = () => {
           <p className="mt-6 font-bold text-xl sm:text-2xl leading-[38px] text-[#000000]">
             2. When to use Stash
           </p>
-          <p className="mt-3 text-xl text-[#000000] font-normal font-['PT_Serif']">
+          <p className="mt-3 text-md sm:text-lg md:text-xl text-[#000000] font-normal font-['PT_Serif']">
             Lacinia ut gravida tempus sollicitudin condimentum sit urna cursus
             malesuada. Eu lacus dis et tempor non. Odio tristique dignissim
             lorem <span className="text-[#034AFF]"> ipsum.com </span>
@@ -91,45 +90,48 @@ const ArticleDetails = () => {
           <p className="mt-6 font-bold text-xl sm:text-2xl leading-[38px] text-[#000000]">
             3. How to use Stash
           </p>
-          <p className="mt-1 text-xl text-[#000000] font-normal font-['PT_Serif']">
+          <p className="mt-1 text-md sm:text-lg md:text-xl text-[#000000] font-normal font-['PT_Serif']">
             Lacinia ut gravida tempus sollicitudin condimentum sit urna cursus
             malesuada. Eu lacus dis et tempor non. Odio
             <span className="ml-4 p-1 bg-[#EDEDED] rounded-lg text-sm text-[#000000]">
               index.html
             </span>
           </p>
-          <p className="mt-1 text-xl text-[#000000] font-normal font-['PT_Serif']">
+          <p className="mt-1 text-md sm:text-lg md:text-xl text-[#000000] font-normal font-['PT_Serif']">
             Mauris vestibulum mauris fermentum ultrices amet. Sed neque amet
             viverra nibh.
           </p>
-          <p className="mt-1 text-xl text-[#000000] font-normal font-['PT_Serif']">
+          <p className="mt-1 text-md sm:text-lg md:text-xl text-[#000000] font-normal font-['PT_Serif']">
             Tempus cras pretium ullamcorper nulla eu cursus gravida. Sed tellus
             sapien mauris.
           </p>
 
-          <div className="mt-5 w-full h-[225px] relative">
+          {/* <div className="mt-5 w-full h-[225px] relative">
             <Image src={ArtSS1} layout="fill" className="object-cover" />
-          </div>
-          <p className="mt-1 text-xl text-[#000000] font-normal font-['PT_Serif']">
+          </div> */}
+          <div className="xyz mt-5"></div>
+          <p className="mt-1 text-md sm:text-lg md:text-xl text-[#000000] font-normal font-['PT_Serif']">
             Augue laoreet vulputate risus leo lacus consectetur. Sed vitae
             mollis ornare tincidunt arcu lorem auctor. Massa
             <span className="ml-4 p-1 bg-[#EDEDED] rounded-lg text-sm text-[#000000]">
               StyledComponents.js
             </span>
           </p>
-          <p className="mt-1 text-xl text-[#000000] font-normal font-['PT_Serif']">
+          <p className="mt-1 text-md sm:text-lg md:text-xl text-[#000000] font-normal font-['PT_Serif']">
             Proin est ullamcorper non felis sapien dignissim lectus. Varius
             dictumst.
           </p>
-          <p className="mt-1 text-xl text-[#000000] font-normal font-['PT_Serif']">
+          <p className="mt-1 text-md sm:text-lg md:text-xl text-[#000000] font-normal font-['PT_Serif']">
             Molestie commodo tincidunt ultrices quam ut faucibus. Tincidunt leo
             non ac dictum.
           </p>
 
-          <div className="mt-9 w-full h-[225px] relative">
+          {/* <div className="mt-9 w-full h-[225px] relative">
             <Image src={ArtSS2} layout="fill" className="object-cover" />
-          </div>
-          <p className="mt-2 text-xl text-[#000000] font-normal font-['PT_Serif']">
+          </div> */}
+          <div className="abc mt-9"></div>
+
+          <p className="mt-2 text-md sm:text-lg md:text-xl text-[#000000] font-normal font-['PT_Serif']">
             Lacinia ut gravida tempus sollicitudin condimentum sit urna cursus
             malesuada. Eu lacus dis et tempor non. Odio tristique dignissim
             lorem ipsum vestibulum. Cras quisque praesent.
@@ -141,13 +143,13 @@ const ArticleDetails = () => {
             </p>
             <div className="mt-4 gap-4 flex">
               <Avatar
-                src={ArtProfile}
-                alt="ArtProfile"
                 sx={{
                   width: "50px",
                   height: "50px",
                 }}
-              />
+              >
+                <Image src={ArtProfile} alt="ArtProfile" layout="fill" />
+              </Avatar>
               <div className="flex flex-col justify-center">
                 <p className="font-bold text-base text-[#000000]">
                   3 Years of VueJS (2021) Link Only
@@ -159,13 +161,13 @@ const ArticleDetails = () => {
             </div>
             <div className="mt-7 gap-4 flex">
               <Avatar
-                src={ArtProfile}
-                alt="ArtProfile"
                 sx={{
                   width: "50px",
                   height: "50px",
                 }}
-              />
+              >
+                <Image src={ArtProfile} alt="ArtProfile" layout="fill" />
+              </Avatar>
               <div className="flex flex-col justify-center">
                 <p className="font-bold text-base text-[#000000]">
                   How to choose colors and fonts correctly for your website
@@ -177,13 +179,13 @@ const ArticleDetails = () => {
             </div>
             <div className="mt-7 gap-4 flex">
               <Avatar
-                src={ArtProfile}
-                alt="ArtProfile"
                 sx={{
                   width: "50px",
                   height: "50px",
                 }}
-              />
+              >
+                <Image src={ArtProfile} alt="ArtProfile" layout="fill" />
+              </Avatar>
               <div className="flex flex-col justify-center">
                 <p className="font-bold text-base text-[#000000]">
                   Does PHP still live in 2021 despite of Wordpress and Laravel?
